38b0d80fd68401957ed6bdd1ee07d7294c9eee37
purvaraut21 <98023071+purvaraut21@users.noreply.github.com>
2025-08-16 15:47:49 +0530

    Add files via upload

---
+++ Podcasts Player/music-list.js
@@ -0,0 +1,40 @@
+let allMusic = [
+
+    {
+        name: "HOW TO BE CALM IN EVERY SITUATION",
+        artist: "Speech by Sadguru",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "DESTINY",
+        artist: "Speech by GaurGopalDas",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "SUCCESS AND FAILURE",
+        artist: "Speech by A.P.J Abdul Kalam",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "AASAAN HAI",
+        artist: "Speech by Sandeep Maheshwari",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "REACH UPTO GOALS",
+        artist: "Speech by Rashmika Mandana",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "MENTAL HEALTH",
+        artist: "Speech by Deepika Padukone",
+        img: "music-6",
+        src: "music-6"
+    },
+
+];
